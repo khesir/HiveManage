540df2b72e4b7bf0b71ac4793468c6f6e989b180
SleepingChameleon <rlarombe10@gmail.com>
2025-03-29 09:47:21 +0800

    feat: product.ts done and working

---
+++ src/api/product.ts
@@ -0,0 +1,294 @@
+import {Product} from '@/components/validation/inventory/product';
+import {AxiosError} from 'axios';
+import {create} from 'zustand';
+import {ApiRequest, PaginationResponse, request} from './axios';
+import {ItemRecords} from '@/components/validation/inventory/item-record';
+import {toast} from 'sonner';
+import {ProductSupplier} from '@/components/validation/inventory/product-supplier';
+
+type ProductState = {
+	products: Product[];
+	selectedProduct: Product;
+	loading: boolean;
+	fetchProducts: (params: URLSearchParams) => Promise<void>;
+	getProductById: (productId: number) => Promise<Product | undefined>;
+	addProduct: (newProduct: Omit<Product, 'id'>) => Promise<void>;
+	updateProduct: (
+		productId: number,
+		suppplier_id: number,
+		newProduct: ItemRecords,
+	) => void;
+	deleteProduct: (productId: number) => Promise<void>;
+	updateItemRecords: (
+		productId: number,
+		item_record_id: number,
+		newItem: ItemRecords,
+	) => void;
+	addProductSupplier: (
+		productId: number,
+		newProductSupplier: ProductSupplier,
+	) => void;
+	updateProductSupplier: (
+		productId: number,
+		productSupplierId: number,
+		newProductSupplier: ProductSupplier,
+	) => void;
+	removeProductSupplier: (productId: number, productSupplierId: number) => void;
+	finalize: (productId: number, itemrecordId: Product) => void;
+};
+
+const useProductStore = create<ProductState>((set) => ({
+	products: [],
+	selectedProduct: {} as Product,
+	loading: false,
+
+	fetchProducts: async (params: URLSearchParams) => {
+		set({loading: true});
+		try {
+			const page = Number(params.get('page')) || 1;
+			const pageLimit = Number(params.get('limit')) || 10;
+			const sort = params.get('sort') || null;
+			const offset = (page - 1) * pageLimit;
+
+			const response = await request<PaginationResponse<Product>>(
+				'GET',
+				`/api/v1/ims/product?limit=${pageLimit}&offset=${offset}` +
+					(sort ? `&sort=${sort}` : '') +
+					'&includes=item_records,suppliers',
+			);
+			set({products: response.data});
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.toString());
+			} else if (e instanceof AxiosError) {
+				toast.error(e.response?.data as string);
+			} else {
+				toast.error('An unknown error occured');
+			}
+			console.log(e);
+		} finally {
+			set({loading: false});
+		}
+	},
+	getProductById: async (productId) => {
+		set({loading: true});
+		try {
+			const response = await request<ApiRequest<Product>>(
+				'GET',
+				`/api/v1/ims/product/${productId}?includes=item_records,suppliers`,
+			);
+			let productData: Product;
+			if (!Array.isArray(response.data)) {
+				productData = response.data;
+			} else {
+				productData = response.data[0];
+			}
+			set((state) => ({products: [...state.products, productData as Product]}));
+			set({selectedProduct: productData});
+			return productData;
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.toString());
+			} else if (e instanceof AxiosError) {
+				toast.error(e.response?.data as string);
+			} else {
+				toast.error('An unknown error occured');
+			}
+			console.log(e);
+		} finally {
+			set({loading: false});
+		}
+	},
+	addProduct: async (newProduct: Product) => {
+		set({loading: true});
+		try {
+			await request('POST', `api/v1/ims/product/`, {
+				...newProduct,
+				product_details: (newProduct.product_details ?? '').toString(),
+				product_supplier_id: Number(newProduct.product_suppliers),
+			});
+			toast.success('Product Added');
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.toString());
+			} else if (e instanceof AxiosError) {
+				toast.error(e.response?.data as string);
+			} else {
+				toast.error('An unknown error occured');
+			}
+			console.log(e);
+		} finally {
+			set({loading: false});
+		}
+	},
+	deleteProduct: async (productId) => {
+		set({loading: true});
+		try {
+			await request('DELETE', `api/v1/ims/product/${productId}`);
+			toast.success('Product Deleted');
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.toString());
+			} else if (e instanceof AxiosError) {
+				toast.error(e.response?.data as string);
+			} else {
+				toast.error('An unknown error occured');
+			}
+		} finally {
+			set({loading: false});
+		}
+	},
+	updateProduct: async (
+		productId: number,
+		supplier_id: number,
+		newItem: ItemRecords,
+	) => {
+		set({loading: true});
+		try {
+			await request(
+				'PUT',
+				`/api/v1/ims/product/${productId}/orderProduct/${supplier_id}`,
+				newItem,
+			);
+			toast.success('Product Item Updated successfully');
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.toString());
+			} else if (e instanceof AxiosError) {
+				toast.error(e.response?.data as string);
+			} else {
+				toast.error('An unknown error occured');
+			}
+			console.log(e);
+		} finally {
+			set({loading: false});
+		}
+	},
+	updateItemRecords: async (
+		productId: number,
+		item_record_id: number,
+		newItem: ItemRecords,
+	) => {
+		set({loading: true});
+		try {
+			await request(
+				'PUT',
+				`/api/v1/ims/order/${productId}/productRecord/${item_record_id}`,
+				newItem,
+			);
+			toast.success('Item Record Updated successfully');
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.toString());
+			} else if (e instanceof AxiosError) {
+				toast.error(e.response?.data as string);
+			} else {
+				toast.error('An unknown error occured');
+			}
+			console.log(e);
+		} finally {
+			set({loading: false});
+		}
+	},
+	addProductSupplier: async (
+		productId: number,
+		newProductSupplier: ProductSupplier,
+	) => {
+		set({loading: true});
+		try {
+			await request(
+				'POST',
+				`/api/v1/ims/product/${productId}/productSupplier`,
+				newProductSupplier,
+			);
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.toString());
+			} else if (e instanceof AxiosError) {
+				toast.error(e.response?.data as string);
+			} else {
+				toast.error('An unknown error occured');
+			}
+		} finally {
+			set({loading: false});
+		}
+	},
+	updateProductSupplier: async (
+		productId: number,
+		productSupplierId: number,
+		newProoductSupplier: ProductSupplier,
+	) => {
+		set({loading: true});
+		try {
+			await request(
+				'PUT',
+				`/api/v1/ims/product/${productId}/productSupplier/${productSupplierId}`,
+				newProoductSupplier,
+			);
+			toast.success('Product Supplier Updated successfully');
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.toString());
+			} else if (e instanceof AxiosError) {
+				toast.error(e.response?.data as string);
+			} else {
+				toast.error('An unknown error occured');
+			}
+			console.log(e);
+		} finally {
+			set({loading: false});
+		}
+	},
+
+	removeProductSupplier: async (
+		productId: number,
+		productsupplierId: number,
+	) => {
+		set({loading: true});
+		try {
+			await request(
+				'DELETE',
+				`/api/v1/ims/product/${productId}/productSupplier/${productsupplierId}`,
+			);
+			toast.success('Order item removed');
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.toString());
+			} else if (e instanceof AxiosError) {
+				toast.error(e.response?.data as string);
+			} else {
+				toast.error('An unknown error occured');
+			}
+			console.log(e);
+		} finally {
+			set({loading: false});
+		}
+	},
+
+	finalize: async (productId: number, productData: Product) => {
+		set({loading: true});
+		try {
+			// Just a temporary solution, For some reason after fetching data that is undefined
+			// Is replaced as null
+			await request(
+				'POST',
+				`/api/v1/ims/product/${productId}/finalize`,
+				productData,
+			);
+			toast.success('Product has been added to records');
+		} catch (e) {
+			if (e instanceof Error) {
+				toast.error(e.toString());
+			} else if (e instanceof AxiosError) {
+				toast.error(e.response?.data as string);
+			} else {
+				toast.error('An unknown error occured');
+			}
+			console.log(e);
+		} finally {
+			set({loading: false});
+		}
+	},
+}));
+
+export default useProductStore;
--- src/modules/inventory/_section/product/view-product.tsx
+++ src/modules/inventory/_section/product/view-product.tsx
@@ -1,6 +1,15 @@
+import useProductStore from '@/api/product';
 import {ViewItemTabDetails} from '../../products/view/item-tab-view';
+import {useEffect} from 'react';
+import {useParams} from 'react-router-dom';
 
 export default function ViewProductRecord() {
+	const {id} = useParams();
+	const {getProductById, selectedProduct} = useProductStore();
+	console.log(selectedProduct);
+	useEffect(() => {
+		getProductById(Number(id));
+	}, []);
 	return (
 		<div className="flex flex-col sm:gap-4">
 			<ViewItemTabDetails />
